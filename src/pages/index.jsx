83b64af3ab8a41d777444862fce873baddc73d47
Zaheen Rakib <124238897+zaheenrakib@users.noreply.github.com>
2025-06-22 01:01:01 +0600

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (  
     <div className="bg-[#2A2A2A]">
       <Banner />
-      <TrendingProducts />
+{/*       <TrendingProducts /> */}
       {/* <PageBannerShop /> */}
       <Slider />
       <DailyDeals />
